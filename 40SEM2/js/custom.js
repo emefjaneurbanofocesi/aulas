80e7040a77803b5eced2686f6bc0b390dae0d272
emefjaneurbanofocesi <59063828+emefjaneurbanofocesi@users.noreply.github.com>
2020-01-23 16:14:57 -0300

    Add files via upload

---
+++ 40SEM2/js/custom.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+
+    // Ativar modal 1
+    $(".overlay-1").click(function(){
+        $('#game-1').modal()
+    })
+
+        // Ativar modal 2
+        $(".overlay-2").click(function(){
+            $('#game-2').modal()
+        })
+
+        // Ativar modal 3
+        $(".overlay-3").click(function(){
+            $('#game-3').modal()
+        })
+
+        // Ativar modal 4
+        $(".overlay-4").click(function(){
+            $('#game-4').modal()
+        })
+
+        // Ativar modal 5
+        $(".overlay-5").click(function(){
+            $('#game-5').modal()
+        })
+
+        // Ativar modal 6
+        $(".overlay-6").click(function(){
+            $('#game-6').modal()
+        })
+
+        // Ativar modal 7
+        $(".overlay-7").click(function(){
+            $('#game-7').modal()
+        })
+
+        // Ativar modal 8
+        $(".overlay-8").click(function(){
+            $('#game-8').modal()
+        })    
+
+        // Ativar modal 9
+        $(".overlay-9").click(function(){
+            $('#game-9').modal()
+        })  
+
+   // Ativar modal 1
+    $(".overlay-11").click(function(){
+        $('#game-11').modal()
+    })
+
+        // Ativar modal 2
+        $(".overlay-12").click(function(){
+            $('#game-12').modal()
+        })
+
+        // Ativar modal 3
+        $(".overlay-13").click(function(){
+            $('#game-13').modal()
+        })
+
+        // Ativar modal 4
+        $(".overlay-14").click(function(){
+            $('#game-14').modal()
+        })
+
+        // Ativar modal 5
+        $(".overlay-15").click(function(){
+            $('#game-15').modal()
+        })
+
+        // Ativar modal 6
+        $(".overlay-16").click(function(){
+            $('#game-16').modal()
+        })
+
+        // Ativar modal 7
+        $(".overlay-17").click(function(){
+            $('#game-17').modal()
+        })
+
+        // Ativar modal 8
+        $(".overlay-18").click(function(){
+            $('#game-18').modal()
+        })     
+});

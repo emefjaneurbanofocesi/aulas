a52e0e42235ca57bf36a64f0b2af8b19b360ed6b
emefjaneurbanofocesi <59063828+emefjaneurbanofocesi@users.noreply.github.com>
2020-01-23 14:40:25 -0300

    Add files via upload

---
+++ 39SEM/js/custom.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+    // Ativar modal 1
+    $(".overlay-1").click(function(){
+        $('#game-1').modal()
+    })
+
+        // Ativar modal 2
+        $(".overlay-2").click(function(){
+            $('#game-2').modal()
+        })
+
+        // Ativar modal 3
+        $(".overlay-3").click(function(){
+            $('#game-3').modal()
+        })
+
+        // Ativar modal 4
+        $(".overlay-4").click(function(){
+            $('#game-4').modal()
+        })
+
+        // Ativar modal 5
+        $(".overlay-5").click(function(){
+            $('#game-5').modal()
+        })
+
+        // Ativar modal 6
+        $(".overlay-6").click(function(){
+            $('#game-6').modal()
+        })
+
+        // Ativar modal 7
+        $(".overlay-7").click(function(){
+            $('#game-7').modal()
+        })
+
+        // Ativar modal 8
+        $(".overlay-8").click(function(){
+            $('#game-8').modal()
+        }) 
+        
+        // Ativar modal 9
+        $(".overlay-9").click(function(){
+            $('#game-9').modal()
+        })   
+
+
+        // Ativar modal 10
+        $(".overlay-10").click(function(){
+            $('#game-10').modal()
+        })   
+
+});
